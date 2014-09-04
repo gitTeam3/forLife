e7760ae50500176a9ad19e087efbdae193c6ea34
ziHao <774026531@qq.com>
2014-09-04 21:59:59 +0800

    nothing

--- appProject/pswMonitor/src/com/gitTeam3/forLife/pswMonitor/core/Core.java
+++ appProject/pswMonitor/src/com/gitTeam3/forLife/pswMonitor/core/Core.java
@@ -14,5 +14,25 @@ public class Core
 	public Core(long seed){
 		this.pswSeed=seed;
 	}
-	
+	/**
+	 *返回默认长度的密码
+	 */
+	public String getPsw(){
+		
+		return null;
+	}
+	/**
+	 *根据lengt返回定长的纯数字密码
+	 */
+	public String getPsw(int length){
+
+		return null;
+	}
+	/**
+	 *返回长度为length包含letterLength个字母的混合密码
+	 */
+	public String getPsw(int length,int letterLength){
+
+		return null;
+	}
 }
