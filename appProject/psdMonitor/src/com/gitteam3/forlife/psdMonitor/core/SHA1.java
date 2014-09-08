7cc7b86ecc73ef81b7ceaabe5b362fcd6d787fce
HelloWorld33 <774026531@qq.com>
2014-09-08 17:34:59 +0800

    look by yourself

--- appProject/psdMonitor/src/com/gitteam3/forlife/psdMonitor/core/SHA1.java
+++ appProject/psdMonitor/src/com/gitteam3/forlife/psdMonitor/core/SHA1.java
@@ -5,26 +5,30 @@ import java.security.NoSuchAlgorithmException;
 
 /**
  * Author: zihao
- * Summary:
+ * Summary:算出给定字符串的ＳＨＡ１值
  * Date: 14-9-6
  */
 public class SHA1 {
 
-    public static int[] getAfterSHA1(String inStr) throws NoSuchAlgorithmException {
-        int outBytes[];//最终向外输出的字节数组
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
-        byte digests[] = md.digest(inStr.getBytes());
-        outBytes = new int[(int)digests.length/2];
-        int pointer_L =0;
-        int pointer_R =digests.length-1;
+    public static int[] getAfterSHA1(String inStr){
+        int outBytes[] = null;//最终向外输出的字节数组
+        MessageDigest md = null;//获取加密对象并指定是ＳＨＡ１算法
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            byte digests[] = md.digest(inStr.getBytes());//获得加密后的值
+            outBytes = new int[(int)digests.length/2];
+            int pointer_L =0;
+            int pointer_R =digests.length-1;
 
-        while(pointer_R - pointer_L >= 0){
-            byte l = (byte) Math.abs( digests[pointer_L] );
-            byte r = (byte) Math.abs( digests[pointer_R] );
-            outBytes[pointer_L] = l*r;
-            pointer_L++;
-            pointer_R--;
+            while(pointer_R - pointer_L >= 0){
+                outBytes[pointer_L] = Math.abs( digests[pointer_L] )*Math.abs( digests[pointer_R] );//为了使每个数据的值变大
+                pointer_L++;
+                pointer_R--;
 
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
         return outBytes;
     }
